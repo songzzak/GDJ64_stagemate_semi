6955dfbe3a9f49a8c28556b1334f7e690b733f01
yoonzin <yoonzin@DESKTOP-VUCI2ME>
2023-06-28 19:40:54 +0900

    fix(mypage):회원탈퇴 메소드 이슈 해결

--- src/main/java/com/stagemate/member/controller/WithdrawMemberServlet.java
+++ src/main/java/com/stagemate/member/controller/WithdrawMemberServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.stagemate.admin.service.AdminService;
 import com.stagemate.member.service.MemberService;
 
 
@@ -21,7 +22,7 @@ public class WithdrawMemberServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String id=request.getParameter("id");
-		int result=new MemberService().updateWthdrDate(id);
+		int result=new AdminService().outmember(id);
 		String msg = "";
 		String loc = "/";
 		 if (result > 0) {
