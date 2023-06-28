c77c740d303be3917c55d832b45bcf51edcea32a
user <user@DESKTOP-M8CTEFU>
2023-06-28 11:27:19 +0900

    파일 다운로드 오류 해결, 공지창에 수정기능 추가하는 중입니다.

--- src/main/java/com/stagemate/notice/controller/NoticeListServlet.java
+++ src/main/java/com/stagemate/notice/controller/NoticeListServlet.java
@@ -70,7 +70,7 @@ public class NoticeListServlet extends HttpServlet {
 		}
 		request.setAttribute("pageBar", pageBar);
 		List<Notice> list = new NoticeService().selectNotice(cPage,numPerpage);
-		request.setAttribute("noties", list);
+		request.setAttribute("notices", list);
 		request.getRequestDispatcher("/views/notice/noticeList.jsp").forward(request,response);
 	}
 
